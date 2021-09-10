e7ff7b2b39f47f9035406a4d3dfb6ea3a58ab271
danieltorresg <daniel.torresg@udea.edu.co>
2021-09-10 16:23:14 -0500

    setting cards

--- lab2activities/src/main/java/co/edu/udea/compumovil/gr05_20211/lab2/HomeScreen.java
+++ lab2activities/src/main/java/co/edu/udea/compumovil/gr05_20211/lab2/HomeScreen.java
@@ -21,6 +21,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -42,21 +44,11 @@ public class HomeScreen extends AppCompatActivity {
         preferences = this.getSharedPreferences("sessions",Context.MODE_PRIVATE);
         editor = preferences.edit();
 
-        reciclador = (RecyclerView) findViewById(R.id.reciclador);
-        glm = new GridLayoutManager(this,2);
-        reciclador.setLayoutManager(glm);
-        adapter = new PoisAdapter(listOfPois);
-        reciclador.setAdapter(adapter);
-
         tName = findViewById(R.id.email);
         String email = getIntent().getStringExtra("email");
         tName.setText(email);
         addPoi = findViewById(R.id.addPoi);
-        //onRestart();
-        PoiEntity sitio1 = new PoiEntity("Medellin","Eterna Primavera","https://www.semana.com/resizer/2noyXlnQe0xA-d-VdDa6-acgYhk=/1200x675/filters:format(jpg):quality(50)//cloudfront-us-east-1.images.arcpublishing.com/semana/KI2722K53VBFVN7BR7OMCXSPWM.jpg",4.1f,"11"," ");
-        listOfPois.add(sitio1);
-
-
+        onRestart();
         addPoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,25 +90,44 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     public void onRestart() {
         super.onRestart();
-        //When BACK BUTTON is pressed, the activity on the stack is restarted
-        //Do what you want on the refresh procedure here
+        consultPois();
+    }
 
-        //consultPois();
-
-
+    public void printList(){
+        Log.d("begin" , "----------------------------------------------");
+        if (listOfPois.size() != 0){
+            for (int i = 0 ; i < listOfPois.size() ; i++){
+                Log.d("name" , listOfPois.get(i).getName());
+                Log.d("desc" , listOfPois.get(i).getDescription());
+                //Log.d("picture" , listOfPois.get(i).getPicture());
+                Log.d("rating" , listOfPois.get(i).getRating().toString());
+                Log.d("temperature" , listOfPois.get(i).getTemperature());
+            }
+        }
+        Log.d("begin" , "----------------------------------------------");
+        consultPois();
     }
 
     public void consultPois(){
         UserDatabase userDatabase = UserDatabase.getUserDatabase(getApplicationContext());
         PoiDao poiDao = userDatabase.poiDao();
+        listOfPois = new ArrayList<>();
+        PoiEntity sitio1 = new PoiEntity("Medellin","Eterna Primavera","https://www.semana.com/resizer/2noyXlnQe0xA-d-VdDa6-acgYhk=/1200x675/filters:format(jpg):quality(50)//cloudfront-us-east-1.images.arcpublishing.com/semana/KI2722K53VBFVN7BR7OMCXSPWM.jpg",4.1f,"11","25");
+        listOfPois.add(sitio1);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 //Register POI
-                listOfPois = poiDao.getPois(preferences.getString("userId", ""));
+                List<PoiEntity> listDB = poiDao.getPois(preferences.getString("userId", ""));
+                for (int i = 0 ; i < listDB.size() ; i++){
+                    listOfPois.add(listDB.get(i));
+
+                }
                 Log.d("name" , "----------------------------------------------");
                 if (listOfPois.size() != 0){
                     for (int i = 0 ; i < listOfPois.size() ; i++){
+
+                        Log.d("name" , i+"");
                         Log.d("name" , listOfPois.get(i).getName());
                         Log.d("desc" , listOfPois.get(i).getDescription());
                         //Log.d("picture" , listOfPois.get(i).getPicture());
@@ -127,6 +138,15 @@ public class HomeScreen extends AppCompatActivity {
                 Log.d("name" , "----------------------------------------------");
             }
         }).start();
+        createRecycler();
+    }
+
+    public void createRecycler(){
+        reciclador = (RecyclerView) findViewById(R.id.reciclador);
+        glm = new GridLayoutManager(this,2);
+        reciclador.setLayoutManager(glm);
+        adapter = new PoisAdapter(listOfPois);
+        reciclador.setAdapter(adapter);
 
     }
 }
